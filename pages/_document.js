1160f4930681675c4a40ff36c7ddc53d23d5b97d
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2022-11-18 16:05:46 +0530

    file update

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,14 @@ class MyDocument extends Document {
     const { lang } = this.props;
     return (
       <Html lang={lang}>
-        <Head>
+        <Head> 
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Parisienne&family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" ></link>
+
+
           {/* <!-- Google Tag Manager --> */}
           <script dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
